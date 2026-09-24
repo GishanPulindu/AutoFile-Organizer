49fab57118d67ccffb2b7b354a9568029ed75913
Gishan Pulindu <kagishpd@gmail.com>
2026-09-24 11:00:59 +0530

    Seperated organizer logic from cli interface

--- Main_CLI.py
+++ Main_CLI.py
@@ -5,10 +5,14 @@ from organizer import FileOrganizer
 def main():
     # Get the location of the folder to be organized
     loc = input("Input the location of the folder: ")
+    preview = False
     if not loc:
         print("No folder has been selected.")
         return
-    organizer = FileOrganizer(loc)
+    confirm = input("Do you want to start a dry run? (Y/N) ")
+    if confirm.upper() == "Y":
+        preview = True
+    organizer = FileOrganizer(loc, dry_run=preview)
 
     if not organizer.validate_location():
         return
